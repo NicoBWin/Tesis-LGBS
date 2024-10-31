e6b0b434e5ea1705d09cc71a64d7334cfc8a9683
gonza_linares <glinares@balanz.com>
2024-10-30 23:19:57 -0300

    added tbs

--- Programacion/FPGA_main/benchmark/phase_generator_tb.v
+++ Programacion/FPGA_main/benchmark/phase_generator_tb.v
@@ -15,28 +15,31 @@ always #1 clk = ~clk;
 *   Variable declarations   *
 *****************************
 */  
+    localparam precision = 9;
+
     reg reset;
-    reg read;
-    reg recalibrate;
-    reg sdo;
-    
-    wire cs;
-    wire sclk;
-    wire [11:0] value_read;
+
+    wire[precision-1:0] sin_a;
+    wire[precision-1:0] sin_b;
+    wire[precision-1:0] sin_c;
+
+    wire out_a;
+    wire out_b;
+    wire out_c;
 /*
 *************************************
 *   External Modules declarations   *
 *************************************
 */
-    ADC adc(
-        .clk(clk),            
-        .reset(reset),
-        .read(read),
-        .recalibrate(recalibrate),
-        .sdo(sdo),
-        .cs(cs),
-        .sclk(sclk),
-        .value(value_read)
+    phase_generator #(parameter BITS_PRECISION = precision) gen(
+        .clk(clk)
+        .reset(reset)
+        .phase_a_sin(sin_a),
+        .phase_b_sine(sin_b),
+        .phase_c_sine(sin_c),
+        .phase_a_pwm(out_a),
+        .phase_b_pwm(out_b),
+        .phase_c_pwm(out_c)
     );
 
 /*
@@ -48,13 +51,11 @@ initial begin
         $dumpfile("top_tb.vcd");
         $dumpvars(0, top_tb);   // module to dump
 
-        //Reseteamos el driver
+        //Transmit
         reset <= 1; 
         #10
         reset <= 0;
-
         
-
         #5000
         
         $finish;
