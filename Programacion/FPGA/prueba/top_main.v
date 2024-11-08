d83af2e6ae9d6bf8d662c774ecdb42b61828499a
GullinoAgus <agullino@itba.edu.ar>
2024-11-08 16:34:16 -0300

    read main to test

--- Programacion/FPGA/prueba/top_main.v
+++ Programacion/FPGA/prueba/top_main.v
@@ -11,22 +11,7 @@ module top_main(
     input wire gpio_23,
     input wire gpio_25,
 
-    input wire gpio_34;
-    input wire gpio_43;
-    input wire gpio_36;
-    input wire gpio_42;
-    input wire gpio_38;
-    input wire gpio_28;
-
     output wire gpio_10;
-
-    output wire gpio_12;
-    output wire gpio_21;
-    output wire gpio_13;
-
-    output wire gpio_47;
-    output wire gpio_46;
-    output wire gpio_2;
 );
 
 /*
@@ -36,39 +21,11 @@ module top_main(
 */
 
     wire tx;
-    wire rx;
     wire shoot;
-    wire phase_a_top;
-    wire phase_b_top;
-    wire phase_c_top;
-    wire phase_a_down;
-    wire phase_b_down;
-    wire phase_c_down;
 
-    wire cs_1;
-    wire sdo_1;
-    wire sclk_1;
-    wire cs_2;
-    wire sdo_2;
-    wire sclk_2;
-    
     assign tx = gpio_10;
     assign rx = gpio_23;
     assign shoot = gpio_25;
-
-    assign phase_a_top = gpio_34;
-    assign phase_b_top = gpio_43;
-    assign phase_c_top = gpio_36;
-    assign phase_a_down = gpio_42;
-    assign phase_b_down = gpio_38;
-    assign phase_c_down = gpio_28;
-
-    assign gpio_12 = cs_1;
-    assign gpio_21 = sdo_1;
-    assign gpio_13 = sclk_1;
-    assign gpio_47 = cs_2;
-    assign gpio_46 = sdo_2;
-    assign gpio_2 = sclk_2;
 /*
 *********************
 *   HFClock setup   *
@@ -97,6 +54,8 @@ module top_main(
     // General purpose
     reg reset = 0;
     reg[31:0] counter = 0;
+    reg[11:0] adc_value = 0;
+    reg local_counter = 0;
 
     // UART
     reg start_tx;
@@ -105,13 +64,6 @@ module top_main(
     wire tx_busy;
     wire rx_done;
     wire parity_error;
-
-    // ADC
-    reg read_adc = 0;
-    reg recalibrate = 0;
-    reg [11:0] adc_value_1;
-    reg [11:0] adc_value_2;
-
     
 /*
 *************************************
@@ -137,28 +89,6 @@ module top_main(
         .parity_error(parity_error)
     );
 
-    ADC adc_1(
-        .clk(clk),            
-        .reset(reset),
-        .read(read_adc),
-        .recalibrate(recalibrate),
-        .sdo(sdo_1),
-        .cs(cs_1),
-        .sclk(sclk_1),
-        .value(adc_value_1)
-    );
-
-    ADC adc_2(
-        .clk(clk),            
-        .reset(reset),
-        .read(read_adc),
-        .recalibrate(recalibrate),
-        .sdo(sdo_2),
-        .cs(cs_2),
-        .sclk(sclk_2),
-        .value(adc_value_2)
-    );
-
     defparam transmitter.PARITY = 0;
     defparam receiver.PARITY = 0;
     //defparam transmitter.BAUD_RATE = `BAUD24M;
@@ -172,8 +102,14 @@ module top_main(
 
     parameter INIT  = 3'b001; 
     parameter IDLE = 3'b010;
-    parameter READ = 3'b011;
-    parameter TX = 3'b100; 
+    parameter TX = 3'b100;
+    parameter REQ_ACK = 3'b101;
+    parameter CHECK = 3'b011;
+
+    localparam READ_ADC = 8'b10011011;
+    localparam ACK = 8'b00110011;
+
+    localparam TARGET_CURR = 12’h00F;
 
     reg[2:0] state = INIT;
 
@@ -187,30 +123,52 @@ module top_main(
                 if (counter >= 48000000) begin
                     reset <= 0;
                     counter <= 0;
-                    state <= IDLE;
-                    read_adc <= 1;
+                    state <= REQ_ACK;
                     led_g <= ON;
                 end
             end
 
-            IDLE: begin
-                // Termino la lectura
-                if(cs_1 == 1) begin
-                    data_to_tx <= adc_value_1[11:4];
-                    start_tx <= 1;
+            REQ_ACK: begin
+
+                // Envio un mensaje de leer ADC
+                data_to_tx <= ACK;
+                start_tx <= 1;
+
+                if (rx_done)
                     led_g <= OFF;
-                    led_b <= ON;
-                end
+                    if(data_received == ACK) begin
+                        led_b <= ON;
+                        state <= TX;
+                        start_tx <= 0;
+                    end
+                    else begin
+                        led_r <= ON;
+                    end
             end
 
             TX: begin
-                counter <= counter + 1;
+                
+                data_to_tx <= READ_ADC; //Mandamos el codigo para recibir la lectura
+                start_tx <= 1;
 
-                // 5 sec
-                if (counter >= 48000000*5) begin
-                    state <= INIT;
-                    led_b <= OFF;
-                end
+                if (rx_done)
+                    if (local_counter == 0) begin
+                        adc_value[7:0] <= data_received;
+                        local_counter = local_counter + 1;
+                    end
+                    else begin
+                        adc_value[11:8] <= data_received[3:0];
+                        state <= CHECK;
+                        led_b <= OFF;
+                    end
+            end
+
+            CHECK: begin
+                
+                if (adc_value >= TARGET_CURR)
+                    led_g <= ON;
+                else
+                    led_r <= ON;
             end
 
         endcase
