6732c67719c3ee55d3eda738e0f8db69fcaecb73
Gonzalo Linares <golinares@itba.edu.ar>
2024-12-24 00:33:19 -0300

    minor changes

--- Programacion/FPGA/src/SPI/SPI.v
+++ Programacion/FPGA/src/SPI/SPI.v
@@ -1,119 +1,84 @@
-`include "./src/ADC/ADC.vh"
+`include "./src/SPI/SPI.vh"
 
 module SPI(
-    input wire clk,            
-    input wire reset,
-    input wire read,
-    input wire recalibrate,
+    input wire clk,            // System clock
+    input wire reset,          // System reset
 
-    // Control reg
-    input wire sdo,
-    output reg cs,
-    output wire sclk,
+    input wire [15:0] data_to_rx, // Data to be received from external device
+    output reg [15:0] data_to_tx, // Data to be transmitted to external device
 
-    output wire [11:0] value,
-    output reg read_done,
-
-    output wire state_0,
-    output wire state_1
+    output reg sclk,     // SPI clock
+    input wire miso,     // Master-In Slave-Out
+    output reg cs        // Chip select
 );
 
-    // Config
-    parameter CLK_FREQ = 24000000;
-    parameter COMM_RATE = `SAMPLE2M4_CLK24M;
-    localparam RECEIVE_COUNT = 14;
-    localparam CALIBRATE_COUNT = 32;
+    // Parameters
+    parameter SCLK_FREQ = 4800000; // 4 MHz clock for SPI
+    parameter SYS_FREQ = 48000000; // Assume 50 MHz system clock
 
-    // States
-    localparam INIT      = 2'b00;
-    localparam CALIBRATE = 2'b01;
-    localparam IDLE      = 2'b10;
-    localparam RECEIVE   = 2'b11;
+    // Internal signals
+    reg [15:0] shift_reg;       // Shift register for SPI communication
+    reg [3:0] bit_counter;      // Counter for tracking bits
+    reg sclk_en;                // Enable SCLK toggle
 
-    // Local variables
-    reg [1:0] state = INIT;
-    reg [11:0] signal_val = 0; 
-    reg calibrate_reset;
-    reg receive_reset;
+    // State machine for SPI
+    typedef enum reg [1:0] {
+        IDLE,
+        SELECT,
+        TRANSFER,
+        DESELECT
+    } spi_state_t;
 
-    assign state_0 = state[0];
-    assign state_1 = state[1];
+    spi_state_t state;
 
-    wire inner_clk;
-    wire[$clog2(RECEIVE_COUNT)-1:0] r_counter;
-    wire[$clog2(CALIBRATE_COUNT)-1:0] c_counter;
-
-    // Modules
-    clk_divider #(COMM_RATE) inner_freq(
-        .clk_in(clk),
-        .reset(reset),
-        .clk_out(inner_clk)
-    );
-
-    up_counter #(RECEIVE_COUNT) receive_counter (
-        .clk_in(inner_clk),
-        .reset(receive_reset),
-        .counter(r_counter)
-    );
-
-    up_counter #(CALIBRATE_COUNT) calibrate_counter(
-        .clk_in(inner_clk),
-        .reset(calibrate_reset),
-        .counter(c_counter)
-    );
-
-    assign sclk = inner_clk;
-    assign value = signal_val;
-
-    always @(negedge inner_clk)
-        begin
-            if(reset) begin
-                state <= INIT;
-            end
-            else begin
-                case (state)
-                    INIT: begin
-                        cs <= 1;
-                        read_done <= 0;
-                        calibrate_reset <= 1; //Mantenemos el contador de calibracion en 0
-                        receive_reset <= 1;
-                        state <= CALIBRATE;
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            state <= IDLE;
+            CS <= 2'b11; // Both devices deselected
+            bit_counter <= 0;
+            shift_reg <= 0;
+            data_to_tx <= 16'b0;
+        end else begin
+            case (state)
+                IDLE: begin
+                    if (|data_to_rx) begin // Check if there's data to send
+                        CS <= 2'b10; // Select device 0 (example)
+                        state <= SELECT;
                     end
+                end
 
-                    CALIBRATE: begin
-                        cs <= 0;
-                        calibrate_reset <= 0;
-                        if (c_counter >= CALIBRATE_COUNT-1) begin
-                            state <= IDLE;  //Calibration done
+                SELECT: begin
+                    if (sclk_en) begin
+                        shift_reg <= data_to_rx; // Load data to shift register
+                        bit_counter <= 15;
+                        state <= TRANSFER;
+                    end
+                end
+
+                TRANSFER: begin
+                    if (sclk_en) begin
+                        if (SCLK == 0) begin
+                            // On falling edge, capture MISO and shift data
+                            shift_reg <= {shift_reg[14:0], MISO[0]};
+                        end else begin
+                            // On rising edge, shift out data
+                            data_to_tx <= shift_reg;
+                        end
+
+                        if (bit_counter == 0) begin
+                            state <= DESELECT;
+                        end else begin
+                            bit_counter <= bit_counter - 1;
                         end
                     end
+                end
 
-                    IDLE: begin
-                        read_done <= 0;
-                        receive_reset <= 1;
-                        calibrate_reset <= 1;
-                        cs <= 1;
-                        if (recalibrate) begin
-                            state <= CALIBRATE;
-                        end
-                        else if (read) begin
-                            state <= RECEIVE;
-                        end
-                    end
-
-                    RECEIVE: begin
-                        cs <= 0;
-                        receive_reset <= 0;
-                        if (r_counter > 2) begin    
-                            signal_val <= {signal_val[10:0], sdo};
-                            if (r_counter >= RECEIVE_COUNT-1) begin
-                                state <= IDLE;
-                                read_done <= 1;
-                            end
-                        end
-                    end
-            
-                endcase
-            end
+                DESELECT: begin
+                    CS <= 2'b11; // Deselect all devices
+                    state <= IDLE;
+                end
+            endcase
         end
+    end
+
 endmodule
