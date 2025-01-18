e43240bd7d763d8cbfc9b9f5738ac0f867138301
damiansergi <72512614+damiansergi@users.noreply.github.com>
2025-01-18 12:30:46 -0300

    La modulacion tiene un revamp

--- Matlab/Modulacion_SPWM/HDL_Coder_Var_Loader.m
+++ Matlab/Modulacion_SPWM/HDL_Coder_Var_Loader.m
@@ -4,10 +4,13 @@ clear
 % de la palabra a la salida del primer contador.
 
 cfg.clk_freq = 48e6; %Es 48MHz
-cfg.tri_freq = 250e3;
+cfg.tri_freq = 25e3;
 cfg.mod_number = 3;
 
 cfg.sawtooth_counter = cfg.clk_freq/cfg.tri_freq - 1;
+
+cfg.counter_word_length = ceil(log2(cfg.sawtooth_counter));
+cfg.counter_tri_word_length = cfg.counter_word_length - 1;
 
 cfg.phase_mul_var = (uint16(cfg.sawtooth_counter/cfg.mod_number));
 
@@ -16,5 +19,3 @@ cfg.sawtooth_phase = [0*cfg.phase_mul_var
                       2*cfg.phase_mul_var];
 
 cfg.tri_counter_comp = uint16(cfg.sawtooth_counter / 2 - 1)
-
-%% TODO
