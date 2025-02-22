2ecbc3b29d4086f275641728797007d4653206cf
gullinoagus <agullino@itba.edu.ar>
2025-02-22 12:10:01 -0300

    micro

---
+++ Programacion/uC/Test/Spi_cube_test/Debug/build/Debug/CMakeFiles/Spi_cube_test.dir/subdir.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+OBJ_SRCS += \
+../build/Debug/CMakeFiles/Spi_cube_test.dir/startup_stm32f103xb.s.obj 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ Programacion/uC/Test/Spi_cube_test/build/Debug/CMakeFiles/3.28.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.6.65-1-MANJARO")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.6.65-1-MANJARO")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/gullino18/Documents/Github/Tesis-LGBS/Programacion/uC/Test/Spi_cube_test/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
